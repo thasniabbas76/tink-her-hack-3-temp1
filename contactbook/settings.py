ce4c00fdb0f01991bdf76f21f525a23ddd083cb0
Thasni Abbas <thasniabbas76@gmail.com>
2025-01-26 12:25:47 +0530

    added db

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'contactbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # PostgreSQL engine
+        'NAME': 'contactmanagerthasni',             # Database name
+        'USER': 'contactmanagerthasni_user',        # Database user
+        'PASSWORD': 'JVYzdMxbZR6ogzKcFtZflU1Te7CWrP53',  # Database password
+        'HOST': 'dpg-cuatmc0gph6c73a22ok0-a',       # Database host (provided by Render)
+        'PORT': '5432',                             # Default PostgreSQL port
     }
 }
+
+
 
 
 # Password validation
